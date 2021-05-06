0ba1568c20fb67ea5297676cbee33e610bedeec0
Cris <crismarlowow@gmail.com>
2021-05-06 20:59:10 +0200

    refactor: App.jsx refactored

--- Apps/calc-app/src/App.jsx
+++ Apps/calc-app/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Functions from './components/Functions';
 import MathOperations from './components/MathOperations';
 import Numbers from './components/Numbers';
@@ -7,29 +7,34 @@ import './App.css';
 
 
 const App = () => {
+    const [stack, setStack] = useState("");
+    
+    return (
+        <main className="react-calculator">
+            <Result value= {stack}/>
+            <Numbers onClickNumber={number =>{
+                console.log("Click en number ", number)
+                setStack(number)
+                }}
+            />
 
-    return (<main className="react-calculator">
-        <Result value= {undefined}/>
-        <Numbers onClickNumber={number =>
-            console.log("Click en number ", number)}
-        />
+            <Functions 
+                onContentClear={() =>
+                    console.log("Content Clear")}
+                onDelete={() =>
+                    console.log("onDelete")}
+            />
 
-        <Functions 
-            onContentClear={() =>
-                console.log("Content Clear")}
-            onDelete={() =>
-                console.log("onDelete")}
-        />
-
-        <MathOperations 
-            onClickOperation={operation => 
-                console.log("Operation: ", operation)
-            }
-            onClickEqual={equal =>
-                console.log("Equal: ", equal)
-            }
-        />
-    </main>);
+            <MathOperations 
+                onClickOperation={operation => 
+                    console.log("Operation: ", operation)
+                }
+                onClickEqual={equal =>
+                    console.log("Equal: ", equal)
+                }
+            />
+        </main>
+    );
 };
 
 export default App;
