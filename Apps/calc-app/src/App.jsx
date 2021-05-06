6945a6f7ee179214762c00ad7a23642a1dc9324c
Cris <crismarlowow@gmail.com>
2021-05-06 18:42:04 +0200

    feat: new component created

--- Apps/calc-app/src/App.jsx
+++ Apps/calc-app/src/App.jsx
@@ -1,32 +1,26 @@
 import React from 'react';
-import Button from './components/Button';
+import Functions from './components/Functions';
 import MathOperations from './components/MathOperations';
+import Numbers from './components/Numbers';
 import Result from './components/Result';
 import './App.css';
 
 
 const App = () => {
 
-    const clickHandlerFunction = text => {
-    }
     return (<main className="react-calculator">
         <Result value= {undefined}/>
-        <div className="numbers">
-            <Button text="1" clickHandler={clickHandlerFunction}/>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-        </div>
-        <div className="functions">
-            <button>clear</button>
-            <button>r</button>
-        </div>
+        <Numbers onClickNumber={number =>
+            console.log("Click en number ", number)}
+        />
+
+        <Functions 
+            onContentClear={() =>
+                console.log("Content Clear")}
+            onDelete={() =>
+                console.log("onDelete")}
+        />
+
         <MathOperations 
             onClickOperation={operation => 
                 console.log("Operation: ", operation)
@@ -34,7 +28,6 @@ const App = () => {
             onClickEqual={equal =>
                 console.log("Equal: ", equal)
             }
-        
         />
     </main>);
 };
