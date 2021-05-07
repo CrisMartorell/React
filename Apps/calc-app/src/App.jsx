e223de9d6eff3cc8c27f51af4f0740717b55de14
Cris <crismarlowow@gmail.com>
2021-05-07 19:01:10 +0200

    refactor: Template literal used

--- Apps/calc-app/src/App.jsx
+++ Apps/calc-app/src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
             <Result value= {stack}/>
             <Numbers onClickNumber={number =>{
                 console.log("Click en number ", number)
-                setStack(number)
+                setStack(`${stack}${number}`)
                 }}
             />
 
