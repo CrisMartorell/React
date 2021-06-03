573b1cad74f9be5400ce9734ad060ae8a176e224
Cris <cmartorelllopez@gmail.com>
2021-06-03 21:03:24 +0200

    feat: renderState done

--- Apps/weather-app/src/components/Weather/Weather.jsx
+++ Apps/weather-app/src/components/Weather/Weather.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Typography from '@material-ui/core/Typography'
+import {WiCloud, WiDayCloudy, WiDayFog, WiDaySunny, WiRain} from 'react-icons/wi'
+import {IconContext} from 'react-icons'
 
-const Weather = ({temperature}) => {
+
+const stateByName= {
+    cloud: WiCloud,
+    cloudy: WiDayCloudy,
+    fog: WiDayFog,
+    sunny: WiDaySunny,
+    rain: WiRain
+}
+
+const renderState = state => {
+    let Icon = stateByName[state] !== undefined ? stateByName[state] : stateByName["sunny"]
+
+    return <Icon/>
+}
+
+const Weather = ({temperature, state}) => {
     return (
         <div>
-            <Typography variant="h2">{temperature}</Typography>
+            <IconContext.Provider value={{size:'5em'}}>
+            <WiCloud></WiCloud>
+            </IconContext.Provider>
+            
+            <Typography display="inline" variant="h3">{temperature}</Typography>
         </div>
     )
 }
